8398f06966c3e567d60f3179e99e4acc8fcbdb01
Jochen Autschbach <jochena@buffalo.edu>
2023-06-02 13:33:49 -0400

    After forking the original repo, ran the 2to3 converter on spec-plot-py and updated README.md

--- README.md
+++ README.md
@@ -1,30 +1,31 @@
 spec-gen.py
 ===========
 
-I wrote this spectral broadening tool because I had some trouble understanding
-the units in the previous tool I used.
+Python tool to broaden calculated spectra (energies, intensities) with Gaussian
+or Lorentzian functions. 
 
 What types of spectra can `spec-gen.py` handle?
 
-* Absorption and Circular Dichroism
-* Infrared and Vibrational Circular Dichroism
-* Emission and Circularly Polarized Luminescence (coming soon!)
+* Electronic absorption and Circular Dichroism (CD)
+* Infrared vivrational spectra (IR) and Vibrational Circular Dichroism (VCD)
+* Emission and Circularly Polarized Luminescence (to be added, not yet available)
 
 The input must be in the format:
 
 ```
 #NExcit TypeSpec
-E1 R1
-E2 R2
+E1 I1
+E2 I2
 ...
 ```
 
 Where `NExcit` is an the number of excitations (no space between `#` and number),
-`TypeSpec` can be `[abs,ir,cd,vcd]`, `E` must be in linear unit system (eV and
-cm^{-1} are good choices, nanometer is BAD!), and `R` is in 10^{-40} cgs units only.
-The `--help` option should give you a good preview of the functionality available.
+`TypeSpec` can be one of `abs, ir, cd, vcd`, and `E` must be in units proportional to the energy (energies in eV or wavenumbers in inverse centimeter are good choices; wavelengths in nanometers are *bad*). The intensity should be 
+given as dimensionless oscillator strengths for absorption, or rotatory strengths in 10E-40 cgs units.
+The `--help` option gives a preview of the functionality.
 Some tips:
 
-* Gaussian and Lorentzian Broadening Value should match x-unit.
-* Integration is given in spec-gen.log
-** if it not equal to 1, you will need to modify the quadrature and padding.
+* Gaussian and Lorentzian Broadening Value should match the column-1 unit.
+* Integration is given in spec-gen.log If it not close to 1, you will need to modify the quadrature and padding.
+* You must give at least an input file name and a broadening type and value, 
+otherwise the script won't do anything.
--- spec-gen.py
+++ spec-gen.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """ spec-gen.py -- A simple spectral convolution program for IR, ECD, VCD, and CPL
 Usage:
     spec-gen.py [options]
@@ -17,7 +17,7 @@ Options:
                                             [default=1000]   
     -p, --padding <factor>              How much should you pad the energy range, see README
                                             [default=5]
-"""
+"""    
 
 
 #####################
@@ -37,22 +37,16 @@ Options:
 # Gaussian and Lorentzian Functions are FWHM #
 ########################
 def gauss(sigma, x, x0):
-    return (1.0 / (sigma * sqrt(2.0 * pi))) * exp(
-        -((x - x0) ** 2.0) / (2.0 * sigma**2.0)
-    )
-
+    return (1.0 / (sigma * sqrt(2.0 * pi))) * exp(-(x - x0) ** 2.0 / (2.0 * sigma ** 2.0))
 
 def lorentz(gamma, x, x0):
     return gamma / (2.0 * pi * ((x - x0) ** 2.0 + (gamma / 2.0) ** 2.0))
 
-
 def abs_int(x):
     return 3.48e-2
 
-
 def cd_int(x):
     return 22.97e0 / x
-
 
 def em_int(x):
     return NotImplementedError
@@ -62,7 +56,7 @@ def em_int(x):
 # Necessary Imports #
 #####################
 from docopt import docopt
-from math import exp, pi, sqrt
+from math import exp, pi, sqrt 
 from numpy import arange
 from os.path import isfile
 from sys import exit
@@ -73,69 +67,53 @@ from sys import exit
 #############
 try:
     # Our favorite option parser :)
-    arguments = docopt(__doc__, version="spec-gen.py version 0.0.2")
+    arguments = docopt(__doc__, version='spec-gen.py version 0.0.2')
 
     # Check user input and set values
     #   if gaussian, check sigma, save broaden
-    if arguments["--gaussian"]:
+    if arguments['--gaussian']:
         try:
-            broaden = float(arguments["--gaussian"])
+            broaden = float(arguments['--gaussian'])
         except ValueError as err:
-            exit(
-                "--> Error: {}\nHow to Fix: Gaussian sigma value should be a floating point number.".format(
-                    err
-                )
-            )
+            exit('--> Error: {}\nHow to Fix: Gaussian sigma value should be a floating point number.'.format(err))
     #   if lorentzian, check gamma, save broaden
-    if arguments["--lorentzian"]:
+    if arguments['--lorentzian']:
         try:
-            broaden = float(arguments["--lorentzian"])
+            broaden = float(arguments['--lorentzian'])
         except ValueError as err:
-            exit(
-                "--> Error: {}\nHow to Fix: Lorentzian gamma value should be a floating point number.".format(
-                    err
-                )
-            )
+            exit('--> Error: {}\nHow to Fix: Lorentzian gamma value should be a floating point number.'.format(err))
     #   if quadrature, check points, save num_points, else set default num_points
-    if arguments["--quadrature"]:
+    if arguments['--quadrature']:
         try:
-            num_points = int(arguments["--quadrature"])
+            num_points = int(arguments['--quadrature'])
         except ValueError as err:
-            exit(
-                "--> Error: {}\nHow to Fix: Quadrature value should be an integer number.".format(
-                    err
-                )
-            )
+            exit('--> Error: {}\nHow to Fix: Quadrature value should be an integer number.'.format(err))
     else:
         num_points = 1000
     #   if padding, check padding, save padding, else set default padding
-    if arguments["--padding"]:
+    if arguments['--padding']:
         try:
-            factor = int(arguments["--padding"])
+            factor = int(arguments['--padding'])
             padding = broaden * factor
         except ValueError as err:
-            exit(
-                "--> Error: {}\nHow to Fix: Padding value should be an integer number.".format(
-                    err
-                )
-            )
+            exit('--> Error: {}\nHow to Fix: Padding value should be an integer number.'.format(err))
     else:
         factor = 5
         padding = broaden * factor
-
+    
     # Now we can read input file (let's see if it exists!)
-    if not isfile(arguments["--input"]):
-        exit("--> Error: {} does not exist?".format(arguments["--input"]))
+    if not isfile(arguments['--input']):
+        exit('--> Error: {} does not exist?'.format(arguments['--input']))
 
-    with open(arguments["--input"], "r") as f:
+    with open(arguments['--input'], 'r') as f:
         # First line will read: #NumberOfExcitations TypeOfSpectrum
         #    by TypeOfSpectrum we can use:
         #       abs, ir, cd, ecd, vcd, em, cpl
         #    The TypeOfSpectrum determines how to deal with impulses
         line = f.readline()
         sp = line.split()
-        num_exc = int(sp[0].replace("#", ""))
-        type_spec = sp[1]
+        num_exc = int(sp[0].replace('#',''))
+        type_spec = sp[1] 
 
         # Read energies and impulses
         energies = [None] * num_exc
@@ -153,90 +131,78 @@ try:
     # To generate the results we need some variables:
     #   for_int -- added to the final integral
     #   imp_val -- the impulse value for iteration i
-    #   integral -- the total integral for our broadened spectrum
+    #   integral -- the total integral for our broadened spectrum 
     for_int = 0.0
     imp_val = 0.0
     integral = 0.0
 
     # There are three types of spectra we can broaden, (abs/ir, cd/ecd/vcd, or em/cpl)
     #   and two types of broadening functions gaussian or lorentzian
-    if type_spec in ["abs", "ir"]:
-        if arguments["--gaussian"]:
+    if type_spec in ['abs', 'ir']:
+        if arguments['--gaussian']:
             for i in arange(energy_range[0], energy_range[1] + delta_e, delta_e):
                 imp_val = 0.0
                 for j in range(num_exc):
                     for_int = impulses[j] * gauss(broaden, i, energies[j]) / abs_int(i)
                     imp_val += for_int
                     integral += for_int
-                print("{} {}".format(i, imp_val))
-        elif arguments["--lorentzian"]:
+                print(('{} {}'.format(i, imp_val)))                    
+        elif arguments['--lorentzian']:
             for i in arange(energy_range[0], energy_range[1] + delta_e, delta_e):
                 imp_val = 0.0
                 for j in range(num_exc):
-                    for_int = (
-                        impulses[j] * lorentz(broaden, i, energies[j]) / abs_int(i)
-                    )
+                    for_int = impulses[j] * lorentz(broaden, i, energies[j]) / abs_int(i)
                     imp_val += for_int
                     integral += for_int
-                print("{} {}".format(i, imp_val))
-    elif type_spec in ["cd", "ecd", "vcd"]:
-        if arguments["--gaussian"]:
+                print(('{} {}'.format(i, imp_val)))                    
+    elif type_spec in ['cd', 'ecd', 'vcd']:
+        if arguments['--gaussian']:
             for i in arange(energy_range[0], energy_range[1] + delta_e, delta_e):
                 imp_val = 0.0
                 for j in range(num_exc):
                     for_int = impulses[j] * gauss(broaden, i, energies[j]) / cd_int(i)
                     imp_val += for_int
                     integral += for_int / energies[j]
-                print("{} {}".format(i, imp_val))
-        elif arguments["--lorentzian"]:
+                print(('{} {}'.format(i, imp_val)))                    
+        elif arguments['--lorentzian']:
             for i in arange(energy_range[0], energy_range[1] + delta_e, delta_e):
                 imp_val = 0.0
                 for j in range(num_exc):
                     for_int = impulses[j] * lorentz(broaden, i, energies[j]) / cd_int(i)
                     imp_val += for_int
                     integral += for_int / energies[j]
-                print("{} {}".format(i, imp_val))
-    elif type_spec in ["em", "cpl"]:
+                print(('{} {}'.format(i, imp_val)))                    
+    elif type_spec in ['em', 'cpl']:
         exit("Error: Emission spectra are currently not supported")
-        # if arguments['--gaussian']:
+        #if arguments['--gaussian']:
         #    for i in arange(energy_range[0], energy_range[1] + delta_e, delta_e):
-        # elif arguments['--lorentzian']:
+        #elif arguments['--lorentzian']:
         #    for i in arange(energy_range[0], energy_range[1] + delta_e, delta_e):
         #        print('EM')
     else:
         exit("Error: This program doesn't know how to broaden the input file")
 
-    # Finally, we will print a spec-gen.log for the user
-    with open("spec-gen.log", "w") as f:
-        f.write("This program was written by Barry Moore (moore0557@gmail.com)\n")
-        f.write("-> Please email with any questions/comments/suggestions\n")
-        f.write("-> User is responsible for any and all broadened spectra!\n")
-        f.write(" === DEBUGGING INFORMATION BELOW THIS LINE === \n")
+    # Finally, we will print a spec-gen.log for the user        
+    with open('spec-gen.log', 'w') as f:
+        f.write('This program was written by Barry Moore (moore0557@gmail.com)\n')
+        f.write('-> Please email with any questions/comments/suggestions\n')
+        f.write('-> User is responsible for any and all broadened spectra!\n')
+        f.write(' === DEBUGGING INFORMATION BELOW THIS LINE === \n')
         # Input File and Spectrum Type
-        f.write(
-            "Input File: {}\nSpectrum Type: {}\n".format(
-                arguments["--input"], type_spec
-            )
-        )
+        f.write('Input File: {}\nSpectrum Type: {}\n'.format(arguments['--input'], type_spec))
         # Gaussian/Lorentzian broadening with sigma/gamma
-        if arguments["--gaussian"]:
-            f.write("Broadened with Gaussian functions, sigma = {}\n".format(broaden))
-        elif arguments["--lorentzian"]:
-            f.write("Broadened with Lorentzian functions, gamma = {}\n".format(broaden))
+        if arguments['--gaussian']:
+            f.write('Broadened with Gaussian functions, sigma = {}\n'.format(broaden))
+        elif arguments['--lorentzian']:
+            f.write('Broadened with Lorentzian functions, gamma = {}\n'.format(broaden))
         # Quadrature and Padding
-        f.write(
-            "Number of points in broadened spectrum: {}\nEnergy padding factor: {}\n".format(
-                num_points, factor
-            )
-        )
+        f.write('Number of points in broadened spectrum: {}\nEnergy padding factor: {}\n'.format(num_points, factor))
         # Integration
-        if type_spec in ["abs", "ir"]:
+        if type_spec in ['abs', 'ir']:
             integral = integral * abs_int(1.0) * delta_e / sum(impulses)
-        elif type_spec in ["cd", "ecd", "vcd"]:
+        elif type_spec in ['cd', 'ecd', 'vcd']:
             integral = integral * cd_int(1.0) * delta_e / sum(impulses)
-        f.write(
-            "Integral of broadened spectrum: {} (Should be 1.00)\n".format(integral)
-        )
+        f.write('Integral of broadened spectrum: {} (Should be 1.00)\n'.format(integral))
 
 except KeyboardInterrupt:
-    exit("Interrupt Detected... Exiting")
+    exit('Interrupt Detected... Exiting')
